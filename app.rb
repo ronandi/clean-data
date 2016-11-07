a6429d7578ae56d4ab6a9816a50705932cd854c6
rohit.kumar <rohit.kumar@workday.com>
2016-11-06 17:48:12 -0800

    add current exclusion criteria

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ post '/submit' do
       outputCsvLines << row if row_is_good(row)
     end
 
-    fileOutput = outputCsvLines.join("\n")
+    fileOutput = outputCsvLines.join
     attachment "cleancsv.csv"
     content_type "application/octet-stream"
     fileOutput
@@ -29,7 +29,14 @@ post '/submit' do
 end
 
 def row_is_good row
+  blocked_words = ["do not contact", "leave me alone", "take me off", "remove", "stop texting", "do not call", "do not text",
+                   "already", "early voted", "voted early", "i voted", "wrong person", "wrong number", "fuck", "hell", "asshole",
+                   "shit", "ass", "can't vote", "cannot vote", "don't vote"]
+  blocked_start_words = ["stop", "quit", "no", "dont", "don't"]
   return false if row.field('Inbound/outbound').include?("outbound")
+  return false if row.field('Message').to_s.empty?
+  return false if blocked_start_words.any? { |phrase| row.field('Message').split.first.downcase == phrase }
+  return false if blocked_words.any? { |phrase| row.field('Message').split.first.downcase.include? phrase }
 
   return true
 end

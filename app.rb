f1f2033baf86586236bf0c65d44f5f19b2126cfb
rohit.kumar <rohit.kumar@workday.com>
2016-11-06 16:54:19 -0800

    initial skeleton of building output csv

--- app.rb
+++ app.rb
@@ -1,5 +1,31 @@
 require 'sinatra'
+require 'csv'
 
 get '/' do
   erb :index
+end
+
+post 'submit' do
+  if params[:file] && params[:file][:filename]
+    filename = params[:file][:filename]
+    file = params[:file][:tempfile]
+
+    csv = CSV.parse(file, :headers => true)
+
+    outputCsvLines = Array.new
+
+    csv.each do |row|
+      outputCsvLines << row if (rowIsGood(row))
+    end
+
+  end
+
+end
+
+def row_is_good(row)
+  if (row.field('Inbound/outbound').include?("outbound"))
+      return false
+  end
+
+  return true
 end
